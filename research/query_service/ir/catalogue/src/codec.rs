d087079b1d7c732fc71ae50a1480edaf7413bafb
Melo Yang <meloyang@Melo-Macbook-Air.local>
2022-04-22 16:51:08 +0800

    add a function to pick select range data from vec<u8> code, and add unit tests back

--- research/query_service/ir/catalogue/src/codec.rs
+++ research/query_service/ir/catalogue/src/codec.rs
@@ -18,7 +18,7 @@ use std::collections::BTreeSet;
 use ascii::AsciiString;
 use ascii::ToAsciiChar;
 
-use super::pattern::{Direction, Pattern};
+use super::pattern::Pattern;
 use crate::extend_step::{ExtendEdge, ExtendStep};
 use crate::pattern::PatternEdge;
 pub trait Encode<T> {
@@ -102,6 +102,39 @@ impl Encoder {
         }
 
         return output as u8;
+    }
+
+    pub fn get_decode_value_by_head_tail(
+        src_code: &Vec<u8>, head: u8, tail: u8, storage_unit_bit_num: u8,
+    ) -> i32 {
+        if head < tail {
+            panic!("The head must be at least larger or equal to tail");
+        }
+        let mut output;
+
+        let head_index = src_code.len() - 1 - (head / storage_unit_bit_num) as usize;
+        let head_offset = head % storage_unit_bit_num;
+        let tail_index = src_code.len() - 1 - (tail / storage_unit_bit_num) as usize;
+        let tail_offset = tail % storage_unit_bit_num;
+
+        if head_index >= src_code.len() || tail_index >= src_code.len() {
+            panic!("The head and tail values are out of range");
+        }
+
+        if head_index == tail_index {
+            output = (src_code[head_index] << (8 - 1 - head_offset) >> (8 - 1 - head_offset + tail_offset))
+                as i32;
+        } else {
+            let index_diff = tail_index - head_index;
+            output = (src_code[tail_index] as i32) >> tail_offset;
+            for i in 1..index_diff {
+                output += (src_code[tail_index - i] as i32)
+                    << (storage_unit_bit_num - tail_offset + ((i - 1) as u8) * storage_unit_bit_num);
+            }
+            output += ((src_code[head_index] << (8 - 1 - head_offset) >> (8 - 1 - head_offset)) as i32)
+                << (storage_unit_bit_num - tail_offset + ((index_diff - 1) as u8) * storage_unit_bit_num);
+        }
+        output
     }
 }
 
@@ -190,7 +223,7 @@ impl EncodeUnit {
     pub fn to_vec_u8(&self, storage_unit_bit_num: u8) -> Vec<u8> {
         let storage_unit_num = self.heads[0] / storage_unit_bit_num + 1;
         let mut encode_vec = Vec::with_capacity(storage_unit_num as usize);
-        for i in (0..storage_unit_bit_num).rev() {
+        for i in (0..storage_unit_num).rev() {
             let mut unit_value: u8 = 0;
             for j in 0..self.values.len() {
                 let value = self.values[j];
@@ -306,5 +339,328 @@ impl Encode<AsciiString> for ExtendStep {
             }
         }
         encode_str
+    }
+}
+
+/// Unit Testing
+#[cfg(test)]
+mod tests {
+    use ascii::{self, AsciiString, ToAsciiChar};
+
+    use crate::codec::*;
+    use crate::pattern::*;
+
+    fn build_pattern_testcase_1() -> Pattern {
+        let pattern_edge1 = PatternEdge::new(0, 1, 0, 1, 1, 2);
+        let pattern_edge2 = PatternEdge::new(1, 2, 0, 2, 1, 3);
+        let pattern_vec = vec![pattern_edge1, pattern_edge2];
+        Pattern::from(pattern_vec)
+    }
+
+    fn build_pattern_testcase_2() -> Pattern {
+        let edge_1 = PatternEdge::new(0, 1, 0, 1, 1, 2);
+        let edge_2 = PatternEdge::new(1, 2, 0, 2, 1, 3);
+        let edge_3 = PatternEdge::new(2, 3, 1, 2, 2, 3);
+        let edge_4 = PatternEdge::new(3, 4, 0, 3, 1, 4);
+        let edge_5 = PatternEdge::new(4, 5, 1, 3, 2, 4);
+        let edge_6 = PatternEdge::new(5, 6, 3, 2, 4, 3);
+        let pattern_edges = vec![edge_1, edge_2, edge_3, edge_4, edge_5, edge_6];
+        Pattern::from(pattern_edges)
+    }
+
+    /// ### Generate AsciiString from Vector
+    fn generate_asciistring_from_vec(vec: &Vec<u8>) -> AsciiString {
+        let mut output = AsciiString::new();
+        for value in vec {
+            output.push(value.to_ascii_char().unwrap());
+        }
+
+        output
+    }
+
+    #[test]
+    fn test_create_encode_unit_from_edge() {
+        let pattern = build_pattern_testcase_1();
+        let edge1 = pattern.get_edge_from_id(0);
+        let edge2 = pattern.get_edge_from_id(1);
+        let encoder = Encoder::initialize_from_pattern(&pattern, 5);
+        let encode_unit_1 = EncodeUnit::from_pattern_edge(&pattern, edge1, &encoder);
+        assert_eq!(encode_unit_1.values[0], 1);
+        assert_eq!(encode_unit_1.values[1], 1);
+        assert_eq!(encode_unit_1.values[2], 2);
+        assert_eq!(encode_unit_1.values[3], 0);
+        assert_eq!(encode_unit_1.values[4], 0);
+        let encode_unit_2 = EncodeUnit::from_pattern_edge(&pattern, edge2, &encoder);
+        assert_eq!(encode_unit_2.values[0], 2);
+        assert_eq!(encode_unit_2.values[1], 1);
+        assert_eq!(encode_unit_2.values[2], 3);
+        assert_eq!(encode_unit_2.values[3], 0);
+        assert_eq!(encode_unit_2.values[4], 0);
+    }
+
+    #[test]
+    fn test_initialize_encoder_from_parameter_case1() {
+        let encoder = Encoder::initialize(2, 3, 4, 5);
+        assert_eq!(encoder.edge_label_bit_num, 2);
+        assert_eq!(encoder.vertex_label_bit_num, 3);
+        assert_eq!(encoder.direction_bit_num, 4);
+        assert_eq!(encoder.vertex_index_bit_num, 5);
+    }
+
+    #[test]
+    fn test_initialize_encoder_from_parameter_case2() {
+        let encoder = Encoder::initialize(2, 2, 2, 2);
+        assert_eq!(encoder.edge_label_bit_num, 2);
+        assert_eq!(encoder.vertex_label_bit_num, 2);
+        assert_eq!(encoder.direction_bit_num, 2);
+        assert_eq!(encoder.vertex_index_bit_num, 2);
+    }
+
+    #[test]
+    fn test_initialize_encoder_from_pattern_case1() {
+        let pattern = build_pattern_testcase_1();
+        let default_vertex_index_bit_num = 0;
+        let encoder = Encoder::initialize_from_pattern(&pattern, default_vertex_index_bit_num);
+        assert_eq!(encoder.edge_label_bit_num, 1);
+        assert_eq!(encoder.vertex_label_bit_num, 2);
+        assert_eq!(encoder.direction_bit_num, 2);
+        assert_eq!(encoder.vertex_index_bit_num, 1);
+    }
+
+    #[test]
+    fn test_initialize_encoder_from_pattern_case2() {
+        let pattern = build_pattern_testcase_2();
+        let default_vertex_index_bit_num = 2;
+        let encoder = Encoder::initialize_from_pattern(&pattern, default_vertex_index_bit_num);
+        assert_eq!(encoder.edge_label_bit_num, 3);
+        assert_eq!(encoder.vertex_label_bit_num, 2);
+        assert_eq!(encoder.direction_bit_num, 2);
+        assert_eq!(encoder.vertex_index_bit_num, 2);
+    }
+
+    #[test]
+    fn encode_unit_to_ascii_string() {
+        let pattern = build_pattern_testcase_1();
+        let edge1 = pattern.get_edge_from_id(0);
+        let edge2 = pattern.get_edge_from_id(1);
+        let encoder = Encoder::initialize(2, 2, 2, 2);
+        let encode_unit_1 = EncodeUnit::from_pattern_edge(&pattern, edge1, &encoder);
+        let encode_string_1 = <EncodeUnit as Encode<AsciiString>>::encode_to(&encode_unit_1, &encoder);
+        let expected_encode_string_1: AsciiString = generate_asciistring_from_vec(&vec![2, 96]);
+        assert_eq!(encode_string_1.len(), 2);
+        assert_eq!(encode_string_1, expected_encode_string_1);
+        let encode_unit_2 = EncodeUnit::from_pattern_edge(&pattern, edge2, &encoder);
+        let encode_string_2 = <EncodeUnit as Encode<AsciiString>>::encode_to(&encode_unit_2, &encoder);
+        let expected_encode_string_2: AsciiString = generate_asciistring_from_vec(&vec![4, 112]);
+        assert_eq!(encode_string_2.len(), 2);
+        assert_eq!(encode_string_2, expected_encode_string_2);
+    }
+
+    #[test]
+    fn encode_unit_to_vec_u8() {
+        let pattern = build_pattern_testcase_1();
+        let edge1 = pattern.get_edge_from_id(0);
+        let edge2 = pattern.get_edge_from_id(1);
+        let encoder = Encoder::initialize(2, 2, 2, 2);
+        let encode_unit_1 = EncodeUnit::from_pattern_edge(&pattern, edge1, &encoder);
+        let encode_vec_1 = <EncodeUnit as Encode<Vec<u8>>>::encode_to(&encode_unit_1, &encoder);
+        let expected_encode_vec_1: Vec<u8> = vec![1, 96];
+        assert_eq!(encode_vec_1.len(), 2);
+        assert_eq!(encode_vec_1, expected_encode_vec_1);
+        let encode_unit_2 = EncodeUnit::from_pattern_edge(&pattern, edge2, &encoder);
+        let encode_vec_2 = <EncodeUnit as Encode<Vec<u8>>>::encode_to(&encode_unit_2, &encoder);
+        let expected_encode_vec_2: Vec<u8> = vec![2, 112];
+        assert_eq!(encode_vec_2.len(), 2);
+        assert_eq!(encode_vec_2, expected_encode_vec_2);
+    }
+
+    #[test]
+    fn encode_pattern_to_asciistring_case_1() {
+        let pattern = build_pattern_testcase_1();
+        let encoder = Encoder::initialize(2, 2, 2, 2);
+        let encode_value = <Pattern as Encode<AsciiString>>::encode_to(&pattern, &encoder);
+        let expected_encode_string_1: AsciiString = generate_asciistring_from_vec(&vec![2, 96]);
+        let expected_encode_string_2: AsciiString = generate_asciistring_from_vec(&vec![4, 112]);
+        let expected_encode_value = expected_encode_string_1 + &expected_encode_string_2;
+        assert_eq!(encode_value, expected_encode_value);
+    }
+
+    #[test]
+    fn encode_pattern_to_vec_u8_case_1() {
+        let pattern = build_pattern_testcase_1();
+        let encoder = Encoder::initialize(2, 2, 2, 2);
+        let encode_vec = <Pattern as Encode<Vec<u8>>>::encode_to(&pattern, &encoder);
+        let mut expected_encode_vec_1: Vec<u8> = vec![1, 96];
+        let expected_encode_vec_2: Vec<u8> = vec![2, 112];
+        expected_encode_vec_1.extend(expected_encode_vec_2);
+        assert_eq!(encode_vec, expected_encode_vec_1);
+    }
+
+    #[test]
+    fn test_get_decode_value_by_head_tail_vec8() {
+        let src_code: Vec<u8> = vec![95, 115, 87];
+        // inside one unit
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 0, 0, 8);
+        assert_eq!(picked_value, 1);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 15, 15, 8);
+        assert_eq!(picked_value, 0);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 1, 0, 8);
+        assert_eq!(picked_value, 3);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 2, 1, 8);
+        assert_eq!(picked_value, 3);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 7, 0, 8);
+        assert_eq!(picked_value, 87);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 15, 8, 8);
+        assert_eq!(picked_value, 115);
+        // neighboring units
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 8, 7, 8);
+        assert_eq!(picked_value, 2);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 9, 6, 8);
+        assert_eq!(picked_value, 13);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 8, 4, 8);
+        assert_eq!(picked_value, 21);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 13, 4, 8);
+        assert_eq!(picked_value, 821);
+        // crossing one unit
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 16, 7, 8);
+        assert_eq!(picked_value, 742);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 16, 6, 8);
+        assert_eq!(picked_value, 1485);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 18, 2, 8);
+        assert_eq!(picked_value, 122069);
+    }
+
+    #[test]
+    fn test_get_decode_value_by_head_tail_asciistring() {
+        let src_code: Vec<u8> = vec![95, 115, 87];
+        // inside one unit
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 0, 0, 7);
+        assert_eq!(picked_value, 1);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 13, 13, 7);
+        assert_eq!(picked_value, 1);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 1, 0, 7);
+        assert_eq!(picked_value, 3);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 2, 1, 7);
+        assert_eq!(picked_value, 3);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 6, 0, 7);
+        assert_eq!(picked_value, 87);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 13, 7, 7);
+        assert_eq!(picked_value, 115);
+        // neighboring units
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 7, 6, 7);
+        assert_eq!(picked_value, 3);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 9, 6, 7);
+        assert_eq!(picked_value, 7);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 8, 4, 7);
+        assert_eq!(picked_value, 29);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 13, 4, 7);
+        assert_eq!(picked_value, 925);
+        // crossing one unit
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 14, 6, 7);
+        assert_eq!(picked_value, 487);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 16, 6, 7);
+        assert_eq!(picked_value, 2023);
+        let picked_value = Encoder::get_decode_value_by_head_tail(&src_code, 18, 2, 7);
+        assert_eq!(picked_value, 130677);
+    }
+
+    #[test]
+    fn test_decode_from_encode_unit_to_vec_u8() {
+        let pattern = build_pattern_testcase_1();
+        let edge1 = pattern.get_edge_from_id(0);
+        let edge2 = pattern.get_edge_from_id(1);
+        let encoder = Encoder::initialize(2, 2, 2, 2);
+        let encode_unit_1 = EncodeUnit::from_pattern_edge(&pattern, edge1, &encoder);
+        let encode_vec_1 = <EncodeUnit as Encode<Vec<u8>>>::encode_to(&encode_unit_1, &encoder);
+        let encode_unit_2 = EncodeUnit::from_pattern_edge(&pattern, edge2, &encoder);
+        let encode_vec_2 = <EncodeUnit as Encode<Vec<u8>>>::encode_to(&encode_unit_2, &encoder);
+        assert_eq!(Encoder::get_decode_value_by_head_tail(&encode_vec_1, 9, 8, 8), edge1.get_label());
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_1, 7, 6, 8),
+            edge1.get_start_vertex_label()
+        );
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_1, 5, 4, 8),
+            edge1.get_end_vertex_label()
+        );
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_1, 3, 2, 8),
+            pattern.get_vertex_index(edge1.get_start_vertex_id())
+        );
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_1, 1, 0, 8),
+            pattern.get_vertex_index(edge1.get_end_vertex_id())
+        );
+        assert_eq!(Encoder::get_decode_value_by_head_tail(&encode_vec_2, 9, 8, 8), edge2.get_label());
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_2, 7, 6, 8),
+            edge2.get_start_vertex_label()
+        );
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_2, 5, 4, 8),
+            edge2.get_end_vertex_label()
+        );
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_2, 3, 2, 8),
+            pattern.get_vertex_index(edge2.get_start_vertex_id())
+        );
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_2, 1, 0, 8),
+            pattern.get_vertex_index(edge2.get_end_vertex_id())
+        );
+    }
+
+    #[test]
+    fn test_decode_from_encode_unit_to_asciistring() {
+        let pattern = build_pattern_testcase_1();
+        let edge1 = pattern.get_edge_from_id(0);
+        let edge2 = pattern.get_edge_from_id(1);
+        let encoder = Encoder::initialize(2, 2, 2, 2);
+        let encode_unit_1 = EncodeUnit::from_pattern_edge(&pattern, edge1, &encoder);
+        let encode_string_1 = <EncodeUnit as Encode<AsciiString>>::encode_to(&encode_unit_1, &encoder);
+        let encode_unit_2 = EncodeUnit::from_pattern_edge(&pattern, edge2, &encoder);
+        let encode_string_2 = <EncodeUnit as Encode<AsciiString>>::encode_to(&encode_unit_2, &encoder);
+        let encode_vec_1: Vec<u8> = encode_string_1
+            .into_iter()
+            .map(|ch| ch.as_byte())
+            .collect();
+        let encode_vec_2: Vec<u8> = encode_string_2
+            .into_iter()
+            .map(|ch| ch.as_byte())
+            .collect();
+        assert_eq!(Encoder::get_decode_value_by_head_tail(&encode_vec_1, 9, 8, 7), edge1.get_label());
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_1, 7, 6, 7),
+            edge1.get_start_vertex_label()
+        );
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_1, 5, 4, 7),
+            edge1.get_end_vertex_label()
+        );
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_1, 3, 2, 7),
+            pattern.get_vertex_index(edge1.get_start_vertex_id())
+        );
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_1, 1, 0, 7),
+            pattern.get_vertex_index(edge1.get_end_vertex_id())
+        );
+        assert_eq!(Encoder::get_decode_value_by_head_tail(&encode_vec_2, 9, 8, 7), edge2.get_label());
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_2, 7, 6, 7),
+            edge2.get_start_vertex_label()
+        );
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_2, 5, 4, 7),
+            edge2.get_end_vertex_label()
+        );
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_2, 3, 2, 7),
+            pattern.get_vertex_index(edge2.get_start_vertex_id())
+        );
+        assert_eq!(
+            Encoder::get_decode_value_by_head_tail(&encode_vec_2, 1, 0, 7),
+            pattern.get_vertex_index(edge2.get_end_vertex_id())
+        );
     }
 }
